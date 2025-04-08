c788b40183c407bdbad8f6fc69b75481417c4730
Sharkgamedev <owen.silva@sharksinteractive.net>
2025-04-07 21:54:01 -0400

    Word game

---
+++ src/lib/spacer.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    export let flex = 1;
+</script>
+
+<div style="flex: {flex}"></div>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,97 +1,221 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import Fish from "$lib/fish.svelte";
-    import Submarine from "$lib/submarine.svelte";
+    import Spacer from "$lib/spacer.svelte";
+    import { words } from "$lib/words";
 
-    let depth = 0;
-    let event: any;
+    let guessList: string[] = [];
+    let word = 'chargement...';
 
-    let input = { x: 0, y: 0 };
+    let guess = '';
+    let valid = true;
 
-    let fish = new Array();
+    let rest = 30;
+    let timer = rest;
+    let score = 0;
 
-    onMount(() => {
-        let interval = setInterval(tick, 10);
+    let active = false;
+    let end = false;
 
-        window.addEventListener('touchend', reset);
-        window.addEventListener('mouseup', reset);
-        window.addEventListener('touchstart', ev => takeInput(ev.touches[0]));
-        window.addEventListener('mousedown', takeInput);
+    let level = 0;
 
-        window.addEventListener('deviceorientation', handleOrientation);
-        function handleOrientation(ev: any) {
-            event = ev;
-        }
+    let content: HTMLElement;
+    let interval: number;
 
-        for (let i = 0; i < 5000; i++) {
-            fish.push({
-                x: `${(Math.random() * 800) * (Math.floor((Math.random() * 2) - 1) == 0 ? -1 : 1)}px`,
-                y: `${(Math.random() * 5000).toFixed(2)}px`,
-            });
+    let size = 100000;
 
-            fish = fish;
-        }
-        console.log(fish);
-    });
+    function start() {
+        interval = setInterval(tick, 1000);
+        word = words[level].word;
+
+        active = true;
+        //window?.visualViewport?.addEventListener("resize", () => {
+            //size = window?.visualViewport?.height ?? window.innerHeight;
+        //});
+    }
 
     function tick() {
-        depth -= 0.75;
+        timer--;
+
+        if (timer <= 0) {
+            if (level == 4) {
+                clearInterval(interval);
+                end = true;
+                return;
+            }
+            
+            level++;
+            timer = 30;
+
+            score += guessList.length;
+            guessList = [];
+
+            word = words[level].word;
+        }
     }
 
-    function takeInput(ev: MouseEvent | Touch) {
-        input = {
-            x: Math.sign(ev.pageX - (document.body.clientWidth / 2)),
-            y: Math.sign(ev.pageY - (document.body.clientHeight / 2))
-        };
+    function submit() {
+        if (!words[level].synonyms.includes(guess.replaceAll('-', '').replaceAll(' ', '').toLowerCase()) || guessList.includes(guess)) {
+            content.classList.remove('shake');
+            valid = true;
+            valid = false;
+            return;
+        }
+        
+        guessList.push(guess);
+        guessList = guessList;
+
+        guess = '';
     }
 
-    function reset() {
-        input = { x: 0, y: 0 };
-    }
+    let phrases = [
+        'come up with as many synonyms as you can',
+        'what is the deeper meaning?',
+        'what is it trying to say?',
+        'how does that make you feel?',
+        'but what does it *really* mean?',
+    ];
 </script>
 
-<div class="strip" style="top: 25px; z-index: 5;">
-    {depth.toFixed(2)}
-    {event?.alpha?.toFixed(2)} {event?.beta?.toFixed(2)} {event?.gamma?.toFixed(2)}
+{#if !active}
+
+<div style="display: flex; flex-direction: column; justify-content: center; align-items: center; max-width: 425px; margin: auto;">
+    <div style="min-height: 30px"></div>
+    <h1>meaning.</h1>
+    <p>
+        You know the irritating feeling when you know what a word means, but can't explain it? <br />
+        Get ready to experience that several times over... <br />
+    </p>
+    <h2>what to expect:</h2>
+    <p>
+        Once you hit start, you will be given a word and then have {rest} seconds to come up with as many
+        synonyms for it as possible - in other words, you have to dig deeper into the true meaning of the word.
+        After the {rest} seconds is up, you'll move onto the next word, 5 total.
+    </p>
+    <div style="min-height: 30px"></div>
+    <button style="aspect-ratio: unset; padding: 10px 15px 10px 15px" on:click={start}>start</button>
 </div>
 
-<div style:--depth={depth.toFixed(2)} class="water">
-    <div style="background-color: #90e4f5; height: 50px;"></div>
-
-    <div style="background-color: rgb(0, 255, 255); height: 10px; width: 100%;"></div>
-
-    {#each fish as pos}
-        <Fish {pos}></Fish>
-    {/each}
-</div>
+{:else if !end}
 
 <div class="strip">
-    <Submarine {input}></Submarine>
+    <Spacer flex={2}></Spacer>
+
+    <h1>{timer}s to ponder:</h1>
+    <h2>{word}</h2>
+    <Spacer flex={2}></Spacer>
+
+    <p>{phrases[level]}</p>
+    <div style="display: flex; flex-direction: row; gap: 15px" bind:this={content} class={valid ? '' : 'shake'}>
+        <input 
+            type="text" bind:value={guess} on:keyup={ev => ev.key == 'Enter' ? submit() : () => {}}
+            style="color: {valid ? 'white' : 'red'}" on:keypress={() => valid = true}
+        >
+        <button on:click={submit} on:touchstart={() => valid = true}>✔</button>
+    </div>
+    <Spacer></Spacer>
+
+    <h3 style="flex: 3">
+        <span>past words:</span> <br />
+        {#each guessList as guess}
+            <span>{guess}</span> <br />
+        {/each}
+    </h3>
+
+    <Spacer flex={5}></Spacer>
 </div>
 
+{:else}
+
+<div style="display: flex; flex-direction: column; justify-content: center; align-items: center; max-width: 425px; margin: auto;">
+    <div style="min-height: 30px"></div>
+    <h1>thanks for playing...</h1>
+    <p>
+        this was thrown together in a matter of four hours or so, thanks to<br />
+        a busy weekend + I've been in a bit of a Ludum Dare rut recently..<br />
+    </p>
+    <h2>so thank you anyway for playing this 'game'</h2>
+    <p>
+        Your final score was <strong>{score}</strong> points!
+    </p>
+    <div style="min-height: 30px"></div>
+</div>
+
+{/if}
+
 <style>
-    div {
+    .strip {
         max-width: 425px;
-        height: 10000vh;
+        height: 100vh;
+
+        background-color: #181d22;
 
         margin: auto;
+        padding: 15px;
+
+        display: flex;
+        flex-direction: column; 
+
+        justify-content: space-between;
+        align-items: center;
     }
 
-    .water {
-        transform: translateY(calc(var(--depth) * 1px));
-        background-color: rgb(0, calc(255 + (var(--depth) * 0.025)), calc(255 + (var(--depth) * 0.005)));
-
-        overflow: hidden;
+    h1, h2, h3 {
+        margin: 0;
     }
 
-    .strip {
-        position: fixed;
-        top: 50px;
-        left: 0;
-        right: 0;
+    h2, h3 {
+        font-weight: normal;
+    }
+
+    button {
+        background-color: #2a303b;
+        border-radius: 12px;
+
+        border: none;
+        outline: none;
+
+        font-size: 1rem;
+        font-weight: bold;
+
+        color: white;
+
+        aspect-ratio: 1 / 1;
+        height: 100%;
+    }
+
+    input {
+        outline: 1px solid #2a303b;
+        border: none;
 
         width: 100%;
-        height: 40px;
+        max-width: 250px;
+
+        padding: 10px 20px 10px 20px;
+        color: white;
+
+        text-align: center;
+
+        font-size: 1rem;
+
+        border-radius: 12px;
+        background-color: #181d22;
+    }
+    
+    .shake {
+        animation: shake 0.5s;
+    }
+
+    @keyframes shake {
+        0% { transform: translateX(1px) }
+        10% { transform: translateX(-1px) }
+        20% { transform: translateX(-3px) }
+        30% { transform: translateX(3px) }
+        40% { transform: translateX(1px) }
+        50% { transform: translateX(-1px) }
+        60% { transform: translateX(-3px) }
+        70% { transform: translateX(3px) }
+        80% { transform: translateX(-1px) }
+        90% { transform: translateX(1px) }
+        100% { transform: translateX(1px) }
     }
 </style>
 
